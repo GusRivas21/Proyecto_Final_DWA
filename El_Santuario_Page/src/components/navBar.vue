eee5094fc0ee351238f8c0eb49aa2babd9c8a4ec
karlos <loboscarlos1704@gmail.com>
2025-11-27 10:31:18 -0600

    añadida responsividad para breakpoints lg y md

--- El_Santuario_Page/src/components/navBar.vue
+++ El_Santuario_Page/src/components/navBar.vue
@@ -1,22 +1,23 @@
 <template>
     <nav class="w-full " style="background-color: #141420;">
-        <div class="w-[80%] mx-auto flex flex-row justify-center sm:justify-between gap-x-4 py-3 ">
+        <div class="w-[80%] mx-auto flex flex-row justify-between gap-x-4 py-3 ">
 
-            <div class="w-1/3 justify-between md:justify-start content-center">
+            <div class="w-1/3 justify-between md:justify-start content-center text-center">
                 <h3 class="inter-text-semi-bold text-white">EL SANTUARIO</h3>
             </div>
 
-            <button @click="isOpen = !isOpen"  class="md:hidden text-3xl text-white justify-end">
-                ☰
-            </button>
 
-            <div class="hidden md:flex w-2/3 justify-between">
-                <div class="flex flex-row gap-x-5 mx-5 justify-center content-center ">
+            <!-- <div class="hidden md:flex w-2/3 justify-around"> -->
+            <div class="flex w-2/3 justify-end md:justify-around">
+                <button @click="isOpen = !isOpen"  class="inline xl:hidden text-3xl text-white justify-end">
+                    ☰
+                </button>
+                <div class="hidden xl:flex flex-row gap-x-5 mx-5 justify-center content-center ">
                     <a class="inter-text-light text-white content-center" href="#">Inicio</a>
                     <a class="inter-text-light text-white content-center" href="#">Figuras</a>
                     <a class="inter-text-light text-white content-center" href="#">Ofertas</a>
                 </div>
-                <div class="flex flex-row gap-x-7">
+                <div class="hidden md:flex lg:justify-end flex-row gap-x-7">
                     <div class="flex flex-row bg-white w-3xs h-auto p-5 rounded-xl text-center justify-end">
                         <i class="fa-solid fa-magnifying-glass fa-2xl leading-none p-0 m-0 content-center justify-end" style="color: #539ff0;"></i>
                     </div>
@@ -27,7 +28,7 @@
                 </div>
             </div>
         </div>
-            <div v-if="isOpen" class="flex flex-col items-center gap-6 pb-4 md:hidden">
+            <div v-if="isOpen" class="flex flex-col items-center gap-6 pb-4 ">
                 <div class="h-px w-100 bg-gray-400"></div>
                 <div class="flex flex-col gap-5 justify-center text-center">
                     <a class="inter-text-light text-white content-center" href="#">Inicio</a>
@@ -35,7 +36,7 @@
                     <a class="inter-text-light text-white content-center" href="#">Ofertas</a>
                 </div>
                 <div class="h-px w-100 bg-gray-400"></div>
-                <div class="flex-col">
+                <div class="flex-col md:hidden">
                     <div class="flex flex-row bg-white w-3xs h-auto p-5 mb-5 rounded-xl text-center justify-end">
                         <i class="fa-solid fa-magnifying-glass fa-2xl leading-none p-0 m-0 content-center justify-end" style="color: #539ff0;"></i>
                     </div>
