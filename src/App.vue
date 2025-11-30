3318e7dd1347cf19015818668c317215f488b6f2
GusRivas21 <gustavo.rivas1270@gmail.com>
2025-11-30 02:59:31 -0600

    reestructura del proyecto

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import navBar from './components/NavbarComponent.vue';
+import footerCmpt from './components/FooterComponent.vue';
+import NavbarComponent from './components/NavbarComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+</script>
+
+<template>
+  <NavbarComponent />
+  <FooterComponent />
+</template>
+
+<style scoped>
+
+</style>
